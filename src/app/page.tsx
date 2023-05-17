70e3e75c1ff40f81d6925944772df0fee5095be4
Gil <gilbertocarvalho1997@gmail.com>
2023-05-17 06:45:02 -0300

    continuacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+import { User } from "lucide-react";
 
 export default function Home() {
   return (
-   <h1>Hello word</h1>
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/estrelas.svg)] bg-cover">
+        <div className="bg-backgroundImage-stripes absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full" />
+        <div className="absolute right-2 top-0 bottom-0 bg-stripes w-2  " />
+
+        {/* conteudo */}
+        <a href="" className="flex itens-center gap-3 text-left hover:text-gray-50">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400"> 
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+            <p className="text-sm leading-snug max-w-[140px]"><span className="underline">Crie sua conta </span> e salve suas memórias!</p>
+        </a>
+        <div></div>
+      </div>
+      <div className="flex flex-col p-16 bg-[url(../assets/estrelas.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">Você ainda não registrou nenhuma lembrança, comece a {" "}
+          <a href="#" className="underline hover:text-gray-50">criar agora</a>!
+        </p>
+        </div>
+      </div>
+    </main>
   )
 }
